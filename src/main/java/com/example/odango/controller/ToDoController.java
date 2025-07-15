1a8e4c8474d16ca5dad205e0431f517ac249fdb8
ogasawara_shoko <ogasawara.shoko@alhinc.jp>
2025-07-15 16:46:36 +0900

    タスク追加処理完了

--- src/main/java/com/example/odango/controller/ToDoController.java
+++ src/main/java/com/example/odango/controller/ToDoController.java
@@ -3,20 +3,26 @@ package com.example.odango.controller;
 import com.example.odango.controller.form.TasksForm;
 import com.example.odango.service.TaskService;
 import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ToDoController {
     @Autowired
     TaskService taskService;
+
+    @Autowired
+    HttpSession session;
 
     @GetMapping("/ToDo")
     public ModelAndView top(@RequestParam(value="searchStart",required=false) String start,
@@ -61,17 +67,29 @@ public class ToDoController {
         TasksForm taskForm = new TasksForm();
         mav.setViewName("/new");
         mav.addObject("formModel", taskForm);
+        setErrorMessage(mav);
         return mav;
     }
 
     /* タスク追加機能 */
     @PostMapping("/ToDo/add")
     public ModelAndView addTask(@Validated @ModelAttribute("formModel") TasksForm taskForm, BindingResult result) {
+        // バリデーション
+        if (result.hasErrors()) {
+            List<String> messages = new ArrayList<>();
+            for (FieldError error : result.getFieldErrors()) {
+                messages.add(error.getDefaultMessage());
+            }
+            session.setAttribute("errorMessages", messages);
+            return new ModelAndView("redirect:/ToDo/new");
+        }
+
         // ステータス:未着手で登録
         taskForm.setStatus((short) 1);
         taskService.saveTask(taskForm);
         return new ModelAndView("redirect:/ToDo");
     }
+
     /*削除処理*/
     @DeleteMapping("/ToDo/delete/{id}")
     public ModelAndView deleteTask(@PathVariable Integer id) {
@@ -85,5 +103,14 @@ public class ToDoController {
         taskService.updateStatus(task);
         mav.setViewName("redirect:/ToDo");
         return mav;
+    }
+
+    /* エラーメッセージ */
+    private void setErrorMessage(ModelAndView mav) {
+        if (session.getAttribute("errorMessages") != null) {
+            mav.addObject("errorMessages", session.getAttribute("errorMessages"));
+            // sessionの破棄
+            session.invalidate();
+        }
     }
 }
